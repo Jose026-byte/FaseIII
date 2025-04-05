ecdf46619c3d675c78e134c4220c6f041f6fac50
Jose026-byte <al07029721@tecmilenio.mx>
2025-04-04 21:25:10 -0600

    Initial commit

--- src/com/mantenimiento/Mantenimiento.java
+++ src/com/mantenimiento/Mantenimiento.java
@@ -1,4 +1,20 @@
 package com.mantenimiento;
 
 public class Mantenimiento {
+    private String maquina;
+    private String tipo; // preventivo o correctivo
+    private String descripcion;
+    private String fecha;
+
+    public Mantenimiento(String maquina, String tipo, String descripcion, String fecha) {
+        this.maquina = maquina;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        return "Máquina: " + maquina + " | Tipo: " + tipo + " | Descripción: " + descripcion + " | Fecha: " + fecha;
+    }
 }
