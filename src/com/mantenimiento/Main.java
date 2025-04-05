c78cad1022c8dd1692e514a604c4f4e1a505cea7
Jose026-byte <al07029721@tecmilenio.mx>
2025-04-04 21:25:10 -0600

    Initial commit

--- src/com/mantenimiento/Main.java
+++ src/com/mantenimiento/Main.java
@@ -1,4 +1,8 @@
 package com.mantenimiento;
 
 public class Main {
+    public static void main(String[] args) {
+        RegistroMantenimientos registro = new RegistroMantenimientos();
+        registro.iniciar();
+    }
 }
